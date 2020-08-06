a5e4b1ff699d30f0333822136e708eb2f4c622ca
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2020-08-06 09:52:33 +0530

    custom directives for opening dropdowns

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,6 +1,32 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
-export class DropDownDirective {}
+export class DropDownDirective {
+  // My solution
+
+  // constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  // @HostListener('click')
+  // toggleOpenClass(event: Event): void {
+  //   if (!this.elementRef.nativeElement.classList.contains('open')) {
+  //     this.renderer.addClass(this.elementRef.nativeElement, 'open');
+  //   } else {
+  //     this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+  //   }
+  // }
+
+  @HostBinding('class.open')
+  isOpen = false;
+
+  @HostListener('click')
+  toggleOpenClass(event: Event): void {
+    this.isOpen = !this.isOpen;
+  }
+}
