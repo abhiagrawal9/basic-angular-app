b709596d6170c086b1a4c6bf90512c4017d4fea7
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2020-09-21 06:43:55 +0530

    restructed the auth folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeResooverService } from './recipes/recipe-resolver.service';
-import { AuthComponent } from './auth/auth/auth.component';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   {
