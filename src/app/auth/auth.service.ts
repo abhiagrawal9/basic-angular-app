05a92f828909089ec9ce0fe26000469c7466d7d7
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2020-09-23 09:42:35 +0530

    added auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -72,6 +72,29 @@ export class AuthService {
       );
   }
 
+  autoLogin(): void {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   logout(): void {
     this.user.next(null);
     this.router.navigate(['/auth']);
@@ -88,6 +111,7 @@ export class AuthService {
     );
     const user = new User(email, localId, idToken, tokenExpirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private errorHandler(errorRes: HttpErrorResponse): Observable<any> {
